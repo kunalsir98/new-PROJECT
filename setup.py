1c93c21ad30d0f4c34eb7abc0e8e3e52b77086d0
kunal chopde <kunalchopde98@gmail.com>
2025-01-13 17:41:10 +0530

    'setup'

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List 
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path)->List[str]:
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('','/n')for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='housing',
+    version='0.0.1',
+    author='kunal',
+    author_email='kunalchopde98@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
+)
